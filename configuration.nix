c32bc16ee57ad885180b01f8a8711075d3efa269
Rosario Pulella <RosarioPulella@gmail.com>
2020-01-28 22:52:51 -0500

    Including home manager broke things.

--- configuration.nix
+++ configuration.nix
@@ -51,7 +51,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    home-manager
   ];
 
   services.throttled.enable = true;
