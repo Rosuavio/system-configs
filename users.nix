b7965f90696cb942ee3170ab6583ac5f006e59bf
Rosario Pulella <RosarioPulella@gmail.com>
2020-01-28 22:48:22 -0500

    Rosario does not need to be in networkmanager

--- users.nix
+++ users.nix
@@ -8,7 +8,7 @@
       name = "rosario";
       description = "Rosario Pulella";
       isNormalUser = true;
-      extraGroups = [ "wheel" "networkmanager" ];
+      extraGroups = [ "wheel" ];
     }
   ];
 }
