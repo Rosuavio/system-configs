08aeb4c8d5aa37ba328a9d2b40a3a770428139d8
Rosario Pulella <RosarioPulella@gmail.com>
2021-07-13 00:26:39 -0400

    Add rosario as trusted user

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,8 @@ in
 
   programs.dconf.enable = true;
 
+  nix.trustedUsers = [ "root" "rosario" ];
+
   nix.binaryCaches = [ "https://nixcache.reflex-frp.org" ];
   nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
 
