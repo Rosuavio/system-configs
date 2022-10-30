6774e6781cd5a533e9f8562419cf0e5b121b0ecf
Rosario Pulella <RosarioPulella@gmail.com>
2022-10-29 23:12:37 -0400

    WIP

--- default.nix
+++ default.nix
@@ -123,6 +123,9 @@ in
 
   nix.trustedUsers = [ "root" ];
 
+  nix.binaryCaches = [ "https://nixcache.reflex-frp.org" ];
+  nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
+
   system.stateVersion = "22.05";
 
   system.autoUpgrade.enable = false;
