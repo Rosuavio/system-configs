3f405530f48b946218fbd93f980de27f911cf2ef
Rosario Pulella <RosarioPulella@gmail.com>
2023-04-30 19:01:17 -0400

    Mixed OCR experimenting
    Switch to use regreet for greeter.
    Use dbus-run-session - for some reason (copying regreet config)
    Moved dbus-update-activation-enviroment to bottom of sway config
    Add nixos module option for configuring system to impove OCR accrucy
    Notes

--- module.nix
+++ module.nix
@@ -1,120 +1,145 @@
 { config, pkgs, lib, ... }:
-{
+let
+  cfg = config;
+in {
   imports =
     [ ./users
     ];
 
-  nixpkgs.config.allowUnfree = true;
-
-  boot = {
-    supportedFilesystems = [ "zfs" ];
-    initrd.supportedFilesystems = [ "zfs" ];
+  options = {
+    ocrOptimiztions = lib.mkEnableOption "ocrOptimiztions";
   };
 
-  hardware = {
-    enableAllFirmware = true;
-    enableRedistributableFirmware = true;
-    opengl = {
+  config = {
+    nixpkgs.config.allowUnfree = true;
+
+    boot = {
+      supportedFilesystems = [ "zfs" ];
+      initrd.supportedFilesystems = [ "zfs" ];
+    };
+
+    hardware = {
+      enableAllFirmware = true;
+      enableRedistributableFirmware = true;
+      opengl = {
+        enable = true;
+        driSupport = true;
+      };
+
+      bluetooth.enable = true;
+      pulseaudio.enable = false;
+    };
+
+    security.polkit.enable = true;
+    security.pam.services.swaylock = {};
+
+    environment.pathsToLink = [ "/share/backgrounds/sway" ];
+
+    xdg.portal = {
       enable = true;
-      driSupport = true;
+      # gtkUsePortal = true;
+      wlr.enable = true;
     };
 
-    bluetooth.enable = true;
-    pulseaudio.enable = false;
-  };
+    fonts.enableDefaultFonts = true;
 
-  security.polkit.enable = true;
-  security.pam.services.swaylock = {};
+    services = {
+      zfs.autoScrub.enable = true;
+      pcscd.enable = true;
+      pipewire = {
+        enable = true;
+        pulse.enable = true;
+        alsa.enable = true;
+      };
+      openssh = {
+        enable = true;
+        gatewayPorts = "clientspecified";
+      };
 
-  environment.pathsToLink = [ "/share/backgrounds/sway" ];
+      fwupd.enable = true;
+      printing.enable = true;
+    };
 
-  xdg.portal = {
-    enable = true;
-    # gtkUsePortal = true;
-    wlr.enable = true;
-  };
+    programs = {
+      dconf.enable = true;
+    };
 
-  fonts.enableDefaultFonts = true;
+    services.dbus.packages = [ pkgs.dconf ];
 
-  services = {
-    zfs.autoScrub.enable = true;
-    pcscd.enable = true;
-    pipewire = {
+    # TODO 2020.01.24 (RP) - Find a way to change the esp to "/esp"
+    boot.loader = {
+      systemd-boot = {
+        enable = true;
+        editor = false;
+      };
+      efi = {
+        # efiSysMountPoint = "/boot/efi";
+        canTouchEfiVariables = true;
+      };
+
+      timeout = null;
+    };
+
+    networking.networkmanager.enable = true;
+
+    environment.etc."NetworkManager/system-connections" = {
+      source = "/persist/etc/NetworkManager/system-connections/";
+    };
+
+    environment.systemPackages = with pkgs; [
+      nix
+    ] ++ lib.optional cfg.ocrOptimiztions pkgs.inconsolata;
+
+    i18n.extraLocaleSettings = {
+      LC_CTYPE = "en_US.UTF-8";
+    };
+
+    time.timeZone = "America/New_York";
+
+    nix.settings = {
+      substituters = [ "https://nixcache.reflex-frp.org" ];
+      trusted-public-keys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
+    };
+
+    services.greetd = {
       enable = true;
-      pulse.enable = true;
-      alsa.enable = true;
+      settings.default_session.command = let
+        greetdConfig = (pkgs.formats.toml {}).generate "regreet-config.toml"
+          (lib.optionalAttrs cfg.ocrOptimiztions {
+            GTK = {
+              font_name = "Inconsolata 16";
+              application_prefer_dark_theme = true;
+            };
+          });
+
+        loginSwayConfig = pkgs.writeText "greetd-sway-config" ''
+          # Changeing the scale can improve OCR in tests scripts
+          # TODO: Make this configuable for the test env
+          # output "Virtual-1" scale 1
+
+          # #wlgreet is currently borken on 22.11, its version does not find wayland libs
+          # would like to use it when posible
+          # https://github.com/NixOS/nixpkgs/pull/210464#issuecomment-1437583852
+          exec "${lib.getExe pkgs.greetd.regreet} --config ${greetdConfig}; swaymsg exit"
+
+          # TODO: This does not seem to be working, invesitage
+          bindsym Mod4+shift+e exec swaynag \
+            -t warning \
+            -m 'What do you want to do?' \
+            -b 'Poweroff' 'systemctl poweroff' \
+            -b 'Reboot' 'systemctl reboot'
+
+          # This is a fix for long load times, invesitage
+          # ref: https://github.com/swaywm/sway/wiki#gtk-applications-take-20-seconds-to-start
+          # ref: https://github.com/swaywm/sway/issues/5732
+          # TODO: Test it
+          exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
+        '';
+      in "${pkgs.dbus}/bin/dbus-run-session ${lib.getExe pkgs.sway} --config ${loginSwayConfig}";
     };
-    openssh = {
-      enable = true;
-      gatewayPorts = "clientspecified";
-    };
 
-    fwupd.enable = true;
-    printing.enable = true;
+    system.stateVersion = "22.11";
+
+    system.autoUpgrade.enable = false;
   };
-
-  programs = {
-    dconf.enable = true;
-  };
-
-  services.dbus.packages = [ pkgs.dconf ];
-
-  # TODO 2020.01.24 (RP) - Find a way to change the esp to "/esp"
-  boot.loader = {
-    systemd-boot = {
-      enable = true;
-      editor = false;
-    };
-    efi = {
-      # efiSysMountPoint = "/boot/efi";
-      canTouchEfiVariables = true;
-    };
-
-    timeout = null;
-  };
-
-  networking.networkmanager.enable = true;
-
-  environment.etc."NetworkManager/system-connections" = {
-    source = "/persist/etc/NetworkManager/system-connections/";
-  };
-
-  environment.systemPackages = with pkgs; [
-    nix
-  ];
-
-  i18n.extraLocaleSettings = {
-    LC_CTYPE = "en_US.UTF-8";
-  };
-
-  time.timeZone = "America/New_York";
-
-  nix.settings = {
-    substituters = [ "https://nixcache.reflex-frp.org" ];
-    trusted-public-keys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
-  };
-
-  services.greetd = {
-    enable = true;
-    settings.default_session.command = let
-      login_sway_config = pkgs.writeText "greetd-sway-config" ''
-        exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
-
-        bindsym Mod4+shift+e exec swaynag \
-          -t warning \
-          -m 'What do you want to do?' \
-          -b 'Poweroff' 'systemctl poweroff' \
-          -b 'Reboot' 'systemctl reboot'
-
-        # #wlgreet is currently borken on 22.11, its version does not find wayland libs
-        # would like to use it when posible
-        # https://github.com/NixOS/nixpkgs/pull/210464#issuecomment-1437583852
-        exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
-      '';
-    in "${pkgs.sway}/bin/sway --config ${login_sway_config}";
-  };
-
-  system.stateVersion = "22.11";
-
-  system.autoUpgrade.enable = false;
 }
