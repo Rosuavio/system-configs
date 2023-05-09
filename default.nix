8712dec29980c9512d84834b6ae9bd63fe1d5fd3
Rosario Pulella <RosarioPulella@gmail.com>
2023-05-09 19:48:35 -0400

    WIP

--- default.nix
+++ default.nix
@@ -4,11 +4,7 @@
 , pkgs ? import nixpkgs {}
 , ...
 }:
-let
-  os = import nixos { configuration = ./test-os.nix; };
-in
 {
-  module = import ./module;
   mkRebuildScript = nixpkgs-path: nixos-config-path:
     pkgs.writeShellScriptBin "nixos-rebuild" ''
         exec ${pkgs.nixos-rebuild}/bin/nixos-rebuild \
@@ -16,5 +12,27 @@ in
           -I nixos-config=${nixos-config-path} \
           "$@"
       '';
-  vm = os.vm;
+  vm =
+    (import nixos {
+      configuration = { config, pkgs, lib, modulesPath, ...}: {
+        imports = [
+          (modulesPath + "/virtualisation/qemu-vm.nix")
+          ./module
+          ./default-config.nix
+        ];
+
+        virtualisation = {
+          memorySize = 1024 * 4;
+          diskSize = 1024 * 2;
+          cores = 2;
+          # NOTE: Blindly coppied from
+          # https://discourse.nixos.org/t/problem-with-sway-in-nixos-rebuild-build-vm-how-to-configure-vm/20263/3
+          qemu.options = [
+            "-vga none"
+            "-device virtio-vga-gl"
+            "-display gtk,gl=on"
+          ];
+        };
+      };
+    }).vm;
 }
