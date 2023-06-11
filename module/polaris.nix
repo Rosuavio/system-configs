4ff91380843cd9de1f6a6f6dc4a39a50478a3bf1
Rosario Pulella <RosarioPulella@gmail.com>
2023-06-10 23:22:00 -0400

    Add polaris config to repo

---
+++ module/polaris.nix
@@ -0,0 +1,82 @@
+{ pkgs, lib, ... }:
+let
+  sources = import ../nix/sources.nix;
+
+  hostName = "polaris";
+
+  inherit (sources) nixos-hardware;
+  suspend-fix = pkgs.writeShellScriptBin "suspend-fix" ''
+    #!/usr/bin/env bash
+    isenabled=`cat /proc/acpi/wakeup | grep $1 | grep -o enabled`
+    if [ $isenabled == "enabled" ]; then
+      echo $1 > /proc/acpi/wakeup
+    else
+      exit
+    fi
+  '';
+in
+{
+  imports =
+    [
+      "${nixos-hardware}/common/cpu/amd"
+      "${nixos-hardware}/common/cpu/amd/pstate.nix"
+      "${nixos-hardware}/common/gpu/amd"
+      "${nixos-hardware}/common/pc/ssd"
+      "${nixos-hardware}/common/pc"
+      "${nixos-hardware}/common/hidpi.nix"
+      ./default.nix
+    ];
+
+  boot.kernelModules = [ "btqca" "btusb" "hci_qca" "hci_uart" "sg" "btintel" ];
+
+  hardware = {
+    openrazer = {
+      enable = true;
+      users = [ "rosario" ];
+    };
+  };
+
+  systemd.services = {
+    # Maybe try to figure out whats going on with this device. Try a bios update.
+    # B550I-AORUS-PRO-AX
+    suspend-fix = {
+      enable = true;
+      wants = [ "multi-user.target" ];
+      after = [ "multi-user.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = lib.getExe suspend-fix + " GPP0";
+      };
+    };
+  };
+
+  services = {
+    openssh = {
+      enable = true;
+      passwordAuthentication = false;
+      kbdInteractiveAuthentication = false;
+    };
+
+    hardware.openrgb = {
+      enable = true;
+      motherboard = "amd";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    ntfs3g
+    fuse3
+  ];
+
+  users.users = {
+    rosario = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDrofEHtfNnlLRqi2zb5XmJOvXPlm6eU6XK5YhTgFnB rosario@pulsar"
+      ];
+    };
+  };
+
+  networking.hostName = hostName;
+  networking.hostId = "6a82d7b3";
+}
